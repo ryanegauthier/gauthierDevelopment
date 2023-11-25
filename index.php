df1fd2fbd26e1133b6069f93e4bc24156f9eb4d6
Ryan Gauthier <ryan.gauthier@openenergysolutions.com>
2023-11-25 09:05:07 -0800

    created index

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+	$title = "GauthierDev";
+	$metaKeywords = "Gauthier, Spokane, WA, Developer, Christian, Creating, Hosting, Northwest, Websites, Development";
+	$metaDescription = "A Christian developer that loves to serve and create. Gauthier Development can also host your websites.";
+	include('includes/head.php');
+	include('includes/processForm.php');
+	?>
+<div class="mainContent">
+	<!-- Header -->
+	<div class="intro-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="intro-message">
+						<h1>Gauthier Development</h1>
+						<h3>Hosing, Developing, Creating</h3>
+						<hr class="intro-divider">
+						<ul class="list-inline intro-social-buttons">
+								<li>
+										<a href="http://bit.ly/2BtzcyN" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-user-plus fa-fw"></i> <span class="network-name">Register</span></a>
+								</li>
+								<!-- <li>
+										<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+								</li>
+								<li>
+										<a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+								</li> -->
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.intro-header -->
+	<!-- Page Content -->
+	<div class="content-section-b" id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5 col-sm-6">
+					<img class="img-responsive" src="img/Logo.png" alt="">
+				</div>
+				<div class="col-lg-7 col-sm-6">
+					<h2 class="section-heading">Gauthier Development</h2>
+					<hr class="section-heading-spacer">
+					<div class="clearfix"></div>
+					<p class="lead">A Christian developer that loves to serve and create. Gauthier Development can also host your websites.</p>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.content-section-b -->
+	<div class="content-section-a" id="scripture">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 col-sm-6">
+				</div>
+				<div class="col-lg-5 col-sm-6">
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.content-section-a -->
+	<div class="content-section-b" id="schedule">
+		<?php // include('includes/schedule.php'); ?>
+	</div>
+	<!-- /.content-section-b -->
+
+
+	<div class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<h2>Connect with Gauthier Development</h2>
+				</div>
+				<div class="col-lg-6">
+					<ul class="list-inline banner-social-buttons pull-left">
+						<!-- <li>
+							<a href="https://www.facebook.com/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook-square"></i> <span class="network-name">Facebook</span></a>
+						</li> -->
+						<!-- <li>
+							<a href="https://twitter.com/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-twitter-square"></i> <span class="network-name">Twitter</span></a>
+						</li> -->
+						<!-- <li>
+							<a href="https://www.instagram.com/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-instagram"></i> <span class="network-name">Instagram</span></a>
+						</li> -->
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.banner -->
+</div>
+<?php include('includes/footer.php'); ?>
